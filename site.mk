ff5dfc86c8887fae18ad1ef76266573748e01062
DO9XE <h.l.lueth@gmail.com>
2015-07-05 18:02:56 +0200

    prepare stable 0.7.1

--- site.mk
+++ site.mk
@@ -25,13 +25,13 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.7.1~exp150620
+DEFAULT_GLUON_RELEASE := 0.7.1~
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
-GLUON_BRANCH ?= experimental
+GLUON_BRANCH ?= stable
 export GLUON_BRANCH
 
 GLUON_TARGET ?= ar71xx-generic
