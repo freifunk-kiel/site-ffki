e0da005b3287acddd74cc22f9bc2a5f6ec2072d1
ruben <localgit@r.z11.de>
2015-11-22 00:06:17 +0100

    Add simple USB support to all models with at least 8MB

--- site.mk
+++ site.mk
@@ -37,6 +37,25 @@ GLUON_SITE_PACKAGES += \
 	kmod-usb-net-dm9601-ether
 endif
 
+#ar71xx-generic
+GLUON_TLWR1043_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_TLWR842_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_TLWDR4300_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_TLWR2543_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_WRT160NL_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_DIR825B1_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_TLWR710_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_GLINET_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_WNDR3700_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_WZRHPG450H_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_WZRHPAG300H_SITE_PACKAGES := mod-usb-core kmod-usb2
+GLUON_ARCHERC7_SITE_PACKAGES := mod-usb-core kmod-usb2
+
+#mpc85xx-generic
+GLUON_TLWDR4900_SITE_PACKAGES := mod-usb-core kmod-usb2
+
+# not found: Netgear WNDR 4300
+
 # if you build only one target use the generic versioning
 # if you build several targets in one go, you have to set the version
 #DEFAULT_GLUON_RELEASE := 0.8~exp$(shell date '+%y%m%d%H%M')
