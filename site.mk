359e32c0fea73746ba15ecc80698f11e94ad3ee8
Daniel Ehlers <danielehlers@mindeye.net>
2017-07-19 22:32:00 +0200

    Rename exp into ngly (nightly)

--- site.mk
+++ site.mk
@@ -39,7 +39,7 @@ GLUON_SITE_PACKAGES += \
 # Always call `make` from the command line with the desired release version!
 # otherwise this is generated:
 #DEFAULT_GLUON_RELEASE := 2016.2.6.2
-DEFAULT_GLUON_RELEASE := 2016.2.6.3~exp$(shell date '+%y%m%d%H%M')
+DEFAULT_GLUON_RELEASE := 2016.2.6.3~ngly$(shell date '+%y%m%d%H%M')
 
 
 # Allow overriding the release number from the command line
