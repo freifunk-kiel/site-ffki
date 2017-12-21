4e351f610b6e5997a2713407bd65f3e111401ae9
rubo77 <github@r.z11.de>
2017-12-21 22:18:53 +0100

    ddhcpd wieder aktiviert

--- site.mk
+++ site.mk
@@ -24,8 +24,8 @@ GLUON_SITE_PACKAGES := \
 	haveged
 # from sargon:
 GLUON_SITE_PACKAGES += \
-	roamguide
-#	ddhcpd
+	roamguide \
+	ddhcpd
 
 # from https://github.com/Freifunk-Nord/eulenfunk-packages
 GLUON_SITE_PACKAGES += \
