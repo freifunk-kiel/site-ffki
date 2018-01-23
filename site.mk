5670e7aba3417ee0d3c5bacee093d25255a7d27c
rubo77 <github@r.z11.de>
2018-01-23 18:55:54 +0100

    ffki-packages wieder drin

--- site.mk
+++ site.mk
@@ -36,9 +36,9 @@ GLUON_SITE_PACKAGES += \
 	gluon-ssid-changer
 
 # from ffki-packages:
-#GLUON_SITE_PACKAGES += \
-#	gluon-config-mode-ppa \
-#	gluon-config-mode-hostname-no-pretty
+GLUON_SITE_PACKAGES += \
+	gluon-config-mode-ppa \
+	gluon-config-mode-hostname-no-pretty
 #	gluon-config-mode-contact-info-anonymous-hint
 
 # from ffm-packages
@@ -147,9 +147,9 @@ USB_PACKAGES_STORAGE := \
 	kmod-nls-koi8r \
 	kmod-nls-utf8
 # from ffki-packages:
-#USB_PACKAGES_STORAGE += \
-#	gluon-usb-media \
-#	gluon-config-mode-usb-media
+USB_PACKAGES_STORAGE += \
+	gluon-usb-media \
+	gluon-config-mode-usb-media
 
 # add addition network drivers and usb stuff only to targes where disk space does not matter
 ifeq ($(GLUON_TARGET),x86-generic)
