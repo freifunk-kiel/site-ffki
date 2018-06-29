368abed4f6d3965116ed97baf2df69afb4dad903
Tobias Schramm <tobleminer@gmail.com>
2018-06-30 01:43:45 +0200

    Add error handling

--- update-git.sh
+++ update-git.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e -o pipefail
+
 function update() {
 	# Kill it with fire
 	rm -rf gluon
@@ -15,7 +17,10 @@ function update() {
 	REMOTE_REF=$(git ls-remote -qht "$GLUON_GIT" | grep "$GLUON_BRANCH" | cut -f1)
 	LOCAL_REF=$(git rev-parse -C gluon HEAD)
 
-	if [[ $REMOTE_REF != $LOCAL_REF ]]; then
+	set +e
+
+	if [[ $? -ne 0 || $REMOTE_REF != $LOCAL_REF ]]; then
+		echo "$REMOTE_REF != $LOCAL_REF, updating"
 		update
 	fi
 )
