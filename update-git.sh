4ac446b1fb43fa1a92e53df14bbda079e7919b0e
Tobias Schramm <tobleminer@gmail.com>
2018-06-30 19:44:11 +0200

    Enhance robustness of revision checking

--- update-git.sh
+++ update-git.sh
@@ -15,7 +15,7 @@ function update() {
 		exit 0
 	fi
 
-	REMOTE_REF=$(git ls-remote -qht "$GLUON_GIT" | grep "$GLUON_BRANCH" | cut -f1)
+	REMOTE_REFS="$(git ls-remote -qht "$GLUON_GIT" | grep "$GLUON_BRANCH" | cut -f1)"
 
 	cd gluon
 	set +e
@@ -24,8 +24,21 @@ function update() {
 	set -e
 	cd ..
 
-	if [[ $GIT_EXIT -ne 0 || $REMOTE_REF != "$LOCAL_REF" ]]; then
-		echo "$REMOTE_REF != $LOCAL_REF, updating"
+	if [[ $GIT_EXIT -ne 0 ]]; then
+		echo "Local repo damaged, forcing update"
 		update
+		exit 0
 	fi
+
+	# There can be multiple objects describing the same state, we need to check them all
+	for ref in $REMOTE_REFS; do
+		echo "Checking $ref == $LOCAL_REF"
+		if [[ "$ref" == "$LOCAL_REF" ]]; then
+			echo "Local repo is up to date"
+			exit 0
+		fi
+	done
+
+	echo "Local state does not reflect latest remote state, updating"
+	update
 )
