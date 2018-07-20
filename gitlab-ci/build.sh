da7a04ba759998ce3ae1ed1a5b77e2e5fc88dd97
rubo77 <github@r.z11.de>
2018-07-20 05:43:11 +0200

    ohne  V=s bauen

--- gitlab-ci/build.sh
+++ gitlab-ci/build.sh
@@ -12,7 +12,7 @@
 # =====================================================================
 
 # Default make options
-MAKEOPTS="V=s -j 4"
+MAKEOPTS="-j 4"
 
 # Default to build all Gluon targets if parameter -t is not set
 TARGETS="ar71xx-generic ar71xx-tiny x86-64" # ar71xx-nand ar71xx-mikrotik mpc85xx-generic x86-generic x86-64"
