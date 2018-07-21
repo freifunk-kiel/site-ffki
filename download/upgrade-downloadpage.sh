90dc5173d8b4d8a009ab0222941309b122de5a3f
rubo77 <github@r.z11.de>
2018-07-22 00:31:55 +0200

    geo branch readme

--- download/upgrade-downloadpage.sh
+++ download/upgrade-downloadpage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set the new release-candidate version here
-CUR=2018.1~exp-215
+CUR=2018.1~geo-215
 wget -k --no-check-certificate http://freifunk.in-kiel.de/firmware-rc.html -O index.html
 # replace the data from the template
 sed -i 's|/.*/sysupgrade/gluon-ffki-<VERSION>|/release-candidate/sysupgrade/gluon-ffki-'$CUR'|g' index.html
