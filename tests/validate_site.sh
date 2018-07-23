405efdd901b4384d2f5a68338149235ede33c6ef
rubo77 <github@r.z11.de>
2018-07-23 23:02:16 +0200

    updage validate.sh and reorder modules

--- tests/validate_site.sh
+++ tests/validate_site.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # validate_site.sh checks if the site.conf is valid json
-GLUON_BRANCH='master'
+GLUON_REPO="https://github.com/TobleMiner/gluon"
+GLUON_BRANCH='feature-autoupdate-proxy'
+GLUON_PACKAGES_BRANCH='master'
 
 P=$(pwd)
 echo "####### validating $P/site.conf ..."
+which lua5.1 
+if [ "$?" == 1 ]; then
+  echo lua5.1 not present!
+  echo install with sudo apt install lua5.1
+  exit 1
+fi
 GLUON_SITEDIR="." lua5.1 tests/site_config.lua
+if [ "$?" == 1 ]; then
+  exit 1
+fi
 
 echo "####### validating $P/make-release.sh ..."
 bash -n $P/make-release.sh
@@ -36,37 +47,51 @@ for feed in $GLUON_SITE_FEEDS; do
     echo "branch $branch_var missing"
     exit 1
   fi
-  git clone -b "$branch" --single-branch "$repo" $feed
+  git clone -b "$branch" --depth 1000 --single-branch "$repo" $feed
   if [ "$?" != "0" ]; then exit 1; fi
   cd $feed
+  echo "git checkout $commit"
   git checkout "$commit"
   if [ "$?" != "0" ]; then exit 1; fi
   cd -
 done
+
+echo "####### downloading github.com/freifunk-gluon/packages ..."
+git clone -b $GLUON_PACKAGES_BRANCH --depth 1  --single-branch https://github.com/freifunk-gluon/packages
+
+echo "####### downloading gluon ..."
 cd $testpath
 git init gluon
 cd gluon
-git remote add origin https://github.com/freifunk-gluon/gluon
+git remote add origin $GLUON_REPO
 git config core.sparsecheckout true
 echo "package/*" >> .git/info/sparse-checkout
-git pull --depth=1 origin $GLUON_BRANCH
+git pull --depth 1 origin $GLUON_BRANCH
 cp -a package/ $testpath/packages
 cd $testpath/packages/package
 
 echo "####### validating GLUON_SITE_PACKAGES from $P/site.mk ..."
-# ignore standard packages:
-sed '/GLUON_RELEASE/,$d' $P/site.mk | egrep -v '(#|G|iwinfo|iptables|haveged)'> $testpath/site.mk.sh
+# ignore non-gluon packages and standard gluon features
+sed '/GLUON_RELEASE/,$d' $P/site.mk | egrep -v '(#|G|iwinfo|iptables|haveged|vim|socat|mesh-batman-adv-1[45]|web-advanced|web-wizard)'> $testpath/site.mk.sh
 sed -i 's/\s\\$//g;/^$/d' $testpath/site.mk.sh
 sed -i 's/gluon-mesh-batman-adv-1[45]/gluon-mesh-batman-adv/g' $testpath/site.mk.sh
 cat $testpath/site.mk.sh |
 while read packet; do
   if [ "$packet" != "" ]; then
-    echo "check $packet ..."
-    if [ "$(find $testpath/packages/ -type d -name "$packet")" '!=' '' ]; then
-      : find found something
+    echo -n "# $packet"
+    FOUND="$(find $testpath/packages/ -type d -name "$packet")"
+    if [ "$FOUND" '!=' '' ]; then
+      echo " found as feature in $(echo $FOUND|sed 's|'$testpath'/packages||g')"
     else
-      echo "ERROR: $packet missing"
-      exit 1
+      # check again with prefix gluon-
+      FOUND="$(find $testpath/packages/ -type d -name "gluon-$packet")"
+      if [ "$FOUND" '!=' '' ]; then
+        echo " found in $(echo $FOUND|sed 's|'$testpath'/packages||g')"
+      else
+        echo
+        echo "ERROR: $packet missing"
+        exit 1
+      fi
     fi
   fi
 done
