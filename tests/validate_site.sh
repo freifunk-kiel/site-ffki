d9c30bb16489a5fecec70ff08e2d74d0e51858a5
rubo77 <github@r.z11.de>
2018-08-29 10:01:04 +0200

    validate: ignore packages starting with "-gluon-" and fix gluon-config-mode-geo-location-with-map

--- tests/validate_site.sh
+++ tests/validate_site.sh
@@ -73,9 +73,10 @@ cd $testpath/packages/package
 
 echo "####### validating GLUON_SITE_PACKAGES from $P/site.mk ..."
 # ignore non-gluon packages and standard gluon features
-sed '0,/^GLUON_LANGS/d' $P/site.mk | sed '/GLUON_TARGET/,$d' | egrep -v '(#|G|iwinfo|iptables|haveged|vim|socat|tar|mesh-batman-adv-1[45]|web-advanced|web-wizard)'> $testpath/site.mk.sh
+sed '0,/^GLUON_LANGS/d' $P/site.mk | sed '/GLUON_TARGET/,$d' | egrep -v '(#|G|iwinfo|iptables|haveged|vim|socat|tar|mesh-batman-adv-1[45]|web-advanced|web-wizard|-gluon-)'> $testpath/site.mk.sh
 sed -i 's/\s\\$//g;/^$/d' $testpath/site.mk.sh
 sed -i 's/gluon-mesh-batman-adv-1[45]/gluon-mesh-batman-adv/g' $testpath/site.mk.sh
+sed -i 's/gluon-config-mode-geo-location-with-map/gluon-config-mode-geo-location/g' $testpath/site.mk.sh
 cat $testpath/site.mk.sh |
 while read packet; do
   if [ "$packet" != "" ]; then
