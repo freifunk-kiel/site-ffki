cfcee11b3eb46367adf49acaea8e906614e6b601
rubo77 <github@r.z11.de>
2018-09-09 18:44:48 +0200

    remove 	config-mode-mesh-vpn (already part of feature web-wizzard)

--- site.mk
+++ site.mk
@@ -25,7 +25,6 @@ GLUON_LANGS ?= en de
 
 # for feature packs see https://github.com/freifunk-gluon/gluon/blob/v2018.1.x/package/features
 GLUON_FEATURES := \
-	config-mode-mesh-vpn \
 	web-private-wifi \
 	ebtables-limit-arp \
 	ebtables-filter-multicast \
