a36472d867a33132f3b71014d25e81a4b54c3632
rubo77 <github@r.z11.de>
2018-10-17 14:11:00 +0200

    reorder targets add broken targets as comments

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -15,7 +15,15 @@
 MAKEOPTS="-j 4"
 
 # Default to build all Gluon targets if parameter -t is not set
-TARGETS="ar71xx-generic ar71xx-tiny ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 mpc85xx-generic ramips-mt7621 sunxi x86-generic x86-geode x86-64 ramips-mt7620 ramips-mt7628 ramips-rt305x"
+TARGETS="ar71xx-generic ar71xx-tiny ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 ramips-mt7621 sunxi x86-generic x86-geode x86-64 ramips-mt7620 ramips-mt7628 ramips-rt305x"
+TARGETS=$TARGETS" mpc85xx-generic" # (tp-link-tl-wdr4900-v1)
+
+# BROKEN:
+#TARGETS=$TARGETS" ramips-mt76x8" # BROKEN: unstable WiFi (tp-link 841 v13 und archer c50)
+#TARGETS=$TARGETS" ar71xx-mikrotik" # BROKEN: no sysupgrade support (mikrotik-nand)
+#TARGETS=$TARGETS" brcm2708-bcm2710" # BROKEN: Untested (raspberry-pi-3)
+#TARGETS=$TARGETS" ipq806x" # BROKEN: unstable wifi drivers (tp-link-archer-c2600)
+#TARGETS=$TARGETS" mvebu-cortexa9" # BROKEN: No AP+IBSS or 11s support (linksys-wrt1200ac)
 
 # Default is set to use current work directory
 SITEDIR="$(pwd)"
