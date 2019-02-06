0c57e1b3971e0d5566bb9498fd6abbf0c712ebcc
heini66 <heini66@users.noreply.github.com>
2019-02-07 00:04:43 +0100

    Update site.mk
    disable button-bind

--- site.mk
+++ site.mk
@@ -69,8 +69,8 @@ GLUON_SITE_PACKAGES += \
 #	gluon-alt-esc-provider
 
 # from ffm-packages
-GLUON_SITE_PACKAGES += \
-	ffffm-button-bind
+# GLUON_SITE_PACKAGES += \
+#	ffffm-button-bind
 
 #	some models and targets have to be excluded:
 ifeq ($(GLUON_TARGET),ar71xx-tiny)
