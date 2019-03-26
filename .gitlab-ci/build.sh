08deac13c9d79cb8b83dd722e73fb8f0b8b3ada1
rubo77 <github@r.z11.de>
2019-03-26 21:42:29 +0100

    Make gluon_release string customizable

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -188,7 +188,7 @@ fi
 
 # Set release number
 if [[ -z "${RELEASE}" ]]; then
-  RELEASE=$(cat "${SITEDIR}/release")
+  RELEASE=$(sed -e "s/BUILD/$BUILD/" "${SITEDIR}/release")
 fi
 
 # Normalize the branch name
@@ -217,7 +217,7 @@ update() {
   make ${MAKEOPTS} \
        GLUON_SITEDIR="${SITEDIR}" \
        GLUON_OUTPUTDIR="${SITEDIR}/output" \
-       GLUON_RELEASE="${RELEASE}-${BUILD}" \
+       GLUON_RELEASE="${RELEASE}" \
        GLUON_BRANCH="${AU_BRANCH}" \
        GLUON_PRIORITY="${PRIORITY}" \
        update
@@ -229,7 +229,7 @@ clean() {
     make ${MAKEOPTS} \
          GLUON_SITEDIR="${SITEDIR}" \
          GLUON_OUTPUTDIR="${SITEDIR}/output" \
-         GLUON_RELEASE="${RELEASE}-${BUILD}" \
+         GLUON_RELEASE="${RELEASE}" \
          GLUON_BRANCH="${AU_BRANCH}" \
          GLUON_PRIORITY="${PRIORITY}" \
          GLUON_TARGET="${TARGET}" \
@@ -243,7 +243,7 @@ download() {
     make ${MAKEOPTS} \
          GLUON_SITEDIR="${SITEDIR}" \
          GLUON_OUTPUTDIR="${SITEDIR}/output" \
-         GLUON_RELEASE="${RELEASE}-${BUILD}" \
+         GLUON_RELEASE="${RELEASE}" \
          GLUON_BRANCH="${AU_BRANCH}" \
          GLUON_PRIORITY="${PRIORITY}" \
          GLUON_TARGET="${TARGET}" \
@@ -259,7 +259,7 @@ build() {
         make ${MAKEOPTS} \
              GLUON_SITEDIR="${SITEDIR}" \
              GLUON_OUTPUTDIR="${SITEDIR}/output" \
-             GLUON_RELEASE="${RELEASE}-${BUILD}" \
+             GLUON_RELEASE="${RELEASE}" \
              GLUON_BRANCH="${AU_BRANCH}" \
              GLUON_PRIORITY="${PRIORITY}" \
              GLUON_TARGET="${TARGET}"
@@ -269,7 +269,7 @@ build() {
         make ${MAKEOPTS} \
              GLUON_SITEDIR="${SITEDIR}" \
              GLUON_OUTPUTDIR="${SITEDIR}/output" \
-             GLUON_RELEASE="${RELEASE}-${BUILD}" \
+             GLUON_RELEASE="${RELEASE}" \
              GLUON_BRANCH="${AU_BRANCH}" \
              GLUON_TARGET="${TARGET}"
       ;;
@@ -280,7 +280,7 @@ build() {
   make ${MAKEOPTS} \
        GLUON_SITEDIR="${SITEDIR}" \
        GLUON_OUTPUTDIR="${SITEDIR}/output" \
-       GLUON_RELEASE="${RELEASE}-${BUILD}" \
+       GLUON_RELEASE="${RELEASE}" \
        GLUON_BRANCH="${BRANCH}" \
        GLUON_PRIORITY="${PRIORITY}" \
        manifest
@@ -310,6 +310,7 @@ sign() {
 }
 
 upload() {
+  set -x
   echo "--- Upload Gluon Firmware Images and Manifest"
 
   # Build the ssh command to use
@@ -326,7 +327,7 @@ upload() {
       mkdir \
           --parents \
           --verbose \
-          "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}-${BUILD}"
+          "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}"
 
   # Add site metadata
   tar -czf "${SITEDIR}/output/images/site.tgz" --exclude='gluon' --exclude='output' "${SITEDIR}"
@@ -339,32 +340,29 @@ upload() {
   echo "Uploading images..."
   rsync \
       --verbose \
-      --recursive \
-      --compress \
       --progress \
-      --links \
       --chmod=ugo=rwX \
       --rsh="${SSH}" \
       "${SITEDIR}/output/images.txz" \
-      "${DEPLOYMENT_USER}@${DEPLOYMENT_SERVER}:${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}-${BUILD}"
+      "${DEPLOYMENT_USER}@${DEPLOYMENT_SERVER}:${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}"
 
   echo "Uncompressing images..."
   ${SSH} \
       ${DEPLOYMENT_USER}@${DEPLOYMENT_SERVER} \
       -- \
-     tar -xJf "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}-${BUILD}/images.txz" -C "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}-${BUILD}/"
+     tar -xJf "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}/images.txz" -C "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}/"
 
   ${SSH} \
       ${DEPLOYMENT_USER}@${DEPLOYMENT_SERVER} \
       -- \
       ln -sf \
-          "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}-${BUILD}/sysupgrade" \
+          "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}/sysupgrade" \
           "${DEPLOYMENT_PATH}/${TARGET}/"
   ${SSH} \
       ${DEPLOYMENT_USER}@${DEPLOYMENT_SERVER} \
       -- \
       ln -sf \
-          "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}-${BUILD}/factory" \
+          "${DEPLOYMENT_PATH}/${TARGET}/${RELEASE}/factory" \
           "${DEPLOYMENT_PATH}/${TARGET}/"
 }
 
@@ -384,7 +382,7 @@ prepare() {
   mv \
     --verbose \
     "${SITEDIR}/output/images" \
-    "${SITEDIR}/output/firmware/${TARGET}/${RELEASE}-${BUILD}"
+    "${SITEDIR}/output/firmware/${TARGET}/${RELEASE}"
 
   # Link latest upload in target to 'current'
   cd "${SITEDIR}/output"
@@ -392,7 +390,7 @@ prepare() {
       --symbolic \
       --force \
       --no-target-directory \
-      "${RELEASE}-${BUILD}" \
+      "${RELEASE}" \
       "firmware/${TARGET}/current"
 }
 
