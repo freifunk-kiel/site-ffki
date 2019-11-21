5458718650a340854bd3330cbcc0eb46192406a9
Ruben Barkow <github@r.z11.de>
2019-11-21 21:07:15 +0100

    in rc: sort targets

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -188,11 +188,8 @@ if [[ -z ${TARGETS+x} ]] ; then
       #TARGETS+=" mvebu-cortexa9" # BROKEN: No AP+IBSS or 11s support (linksys-wrt1200ac)
       ;;
     *)
-      # Default to all targets
-      TARGETS="ar71xx-generic ar71xx-tiny ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 ramips-mt7621 x86-generic x86-geode x86-64 ramips-mt7620 ramips-mt76x8 ramips-rt305x sunxi-cortexa7 ipq40xx"
-      TARGETS+=" mpc85xx-generic" # (tp-link-tl-wdr4900-v1)-
-      TARGETS+=" ipq806x" # BROKEN: unstable wifi drivers (tp-link-archer-c2600)
-      TARGETS+=" mvebu-cortexa9" # BROKEN: No AP+IBSS or 11s support (linksys-wrt1200ac)
+      # rc and stable defaults to all targets
+      TARGETS="ar71xx-generic ar71xx-nand ar71xx-tiny brcm2708-bcm2708 brcm2708-bcm2709 ipq40xx ipq806x mpc85xx-generic mvebu-cortexa9 ramips-mt7620 ramips-mt7621 ramips-mt76x8 ramips-rt305x sunxi-cortexa7 x86-64 x86-generic x86-geode"
     ;;
   esac
 fi
