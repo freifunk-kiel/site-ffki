f1e5a6040729cf44faf86ac48e8f8ef68091766b
Ruben Barkow <github@r.z11.de>
2020-02-18 23:22:18 +0100

    v2020.1 and build all targets with new models

--- make-release.sh
+++ make-release.sh
@@ -15,7 +15,7 @@ set -u
 set -e
 
 # if version is unset, will use the default nightly version from site.mk
-VERSION=${3:-"2020.0~next$(date '+%y%m%d%H%M')"}
+VERSION=${3:-"2020.1~next$(date '+%y%m%d%H%M')"}
 # branch must be set to either rc, nightly or stable
 BRANCH=${2:-"stable"}
 # must point to valid ecdsa signing key created by ecdsakeygen, relative to Gluon base directory
